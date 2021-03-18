43edb7dfd354c9b92b55a05e80f2ae1904ba1286
Zach <fox@highfidelity.com>
2021-03-18 13:44:42 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WebRTC",
-    platforms: [
-        .iOS(.v10)
-    ],
     products: [
         .library(name: "WebRTC", targets: ["WebRTC"]),
     ],
