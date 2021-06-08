ddad37ef1e326d05f8e31e3bc90b3a4d05c472df
Zach Fox <fox@highfidelity.io>
2021-06-08 13:39:33 -0400

    A new start

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    products: [
+        .library(name: "WebRTC", targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "WebRTC", path: "WebRTC.xcframework")
+    ]
+)
